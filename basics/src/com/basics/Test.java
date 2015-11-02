fb984f597f84d8d76357915ee54ed915880a5101
Saurabh Mehta <msaurabh2013@gmail.com>
2015-11-02 12:05:35 -0500

    commit

--- basics/src/com/basics/Test.java
+++ basics/src/com/basics/Test.java
@@ -1,3 +1,5 @@
+package com.basics;
+
 /**
  * Created by Falcon on 11/2/15.
  */
@@ -5,5 +7,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hello for git");
         System.out.println("Second commit");
+        System.out.println("update");
     }
 }
